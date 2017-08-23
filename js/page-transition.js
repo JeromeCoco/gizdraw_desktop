fb182784d46a832847171052ab9ce5f0c024e48c
projpetvet <jeromcoco1@gmail.com>
2017-08-24 03:28:43 +0800

    new canvas UI, settings dropdown fixed

--- js/page-transition.js
+++ js/page-transition.js
@@ -1,10 +1,42 @@
-var recent = document.getElementById('recent');
-var newCanvas = document.getElementById('new-canvas');
+$(document).ready(function(){
 
-recent.onclick = function() {
-	window.location = "index.html";
-}
+	$('#recent-panel').addClass('show');
+	$('#setup-canvas-panel').addClass('hide');
 
-newCanvas.onclick = function() {
-	window.location = "new-canvas.html";
-}
+	function changePanel(){
+		$('.menu-item').removeClass('active');
+		$('.panel').removeClass('show');
+		$('.panel').addClass('hide');
+	}
+
+	//setCanvasColor
+
+	$('#setCanvasColor').change(function(){
+		var set = $(this).val();
+		if (set == "Color") {
+			$(this).after("<input id='colorpick' type='color'/>");
+		} else {
+			$('#colorpick').fadeOut('fast');
+		}
+	});
+
+	$('#recent').click(function(){
+		changePanel();
+		$(this).addClass('active');
+		$('#recent-panel').removeClass('hide');
+		$('#recent-panel').addClass('show');
+	});
+
+	$('#new-canvas').click(function(){
+		changePanel();
+		$(this).addClass('active');
+		$('#setup-canvas-panel').removeClass('hide');
+		$('#setup-canvas-panel').addClass('show');
+	});
+
+	$('#settings').click(function(){
+		$(this).toggleClass('active');
+		$('.sub-settings').toggleClass('show-settings');
+	});
+
+});
