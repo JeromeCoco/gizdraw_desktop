292dee6b808e7ba358d7f26d104ff080905fd04a
projpetvet <jeromcoco1@gmail.com>
2017-09-16 05:01:33 +0800

    backup file

--- server.js
+++ server.js
@@ -7,14 +7,4 @@ console.log("Server is running...");
 
 io.sockets.on('connection', function (socket) {
     console.log('socket connected:' + socket.id);
-
-    /*socket.on("closeModal", function(data){
-    	console.log(data);
-    });*/
-
-    socket.on("message", function(data){
-    	console.log(data);
-    	socket.emit("HelloPC", "hello pc");
-    });
-
 });
